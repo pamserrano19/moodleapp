e2c8feefc90462b50a02b5ba2772aba57aa332ea
pamserrano19 <99166401+pamserrano19@users.noreply.github.com>
2022-06-15 12:11:39 +0800

    Initial Commit
    - LeOnS Configuration
    - Fixed some initial errors/dependencies

---
+++ capacitor.config.ts
@@ -0,0 +1,48 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'moodlemobile',
+  webDir: 'www',
+  bundledWebRuntime: false,
+  cordova: {
+    preferences: {
+      permissions: 'none',
+      orientation: 'default',
+      'target-device': 'universal',
+      fullscreen: 'false',
+      'stay-in-webview': 'false',
+      webviewbounce: 'false',
+      UIWebViewBounce: 'false',
+      DisallowOverscroll: 'true',
+      'prerendered-icon': 'true',
+      AppendUserAgent: 'MoodleMobile 4.0.0-dev (40000)',
+      BackupWebStorage: 'none',
+      ScrollEnabled: 'false',
+      KeyboardDisplayRequiresUserAction: 'false',
+      HideKeyboardFormAccessoryBar: 'false',
+      AllowInlineMediaPlayback: 'true',
+      LoadUrlTimeoutValue: '60000',
+      'load-url-timeout': '60000',
+      SplashScreen: 'screen',
+      SplashScreenDelay: '15000',
+      SplashMaintainAspectRatio: 'true',
+      SplashShowOnlyFirstTime: 'false',
+      'android-minSdkVersion': '22',
+      'android-targetSdkVersion': '30',
+      AndroidPersistentFileLocation: 'Compatibility',
+      AndroidInsecureFileModeEnabled: 'true',
+      CustomURLSchemePluginClearsAndroidIntent: 'true',
+      iosPersistentFileLocation: 'Compatibility',
+      iosScheme: 'moodleappfs',
+      WKWebViewOnly: 'true',
+      WKFullScreenEnabled: 'true',
+      AndroidXEnabled: 'true',
+      GradlePluginGoogleServicesEnabled: 'true',
+      GradlePluginGoogleServicesVersion: '4.3.10',
+      StatusBarOverlaysWebView: 'false'
+    }
+  }
+};
+
+export default config;
